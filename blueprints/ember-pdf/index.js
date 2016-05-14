6d3058cce4ad58b48b54f3605820918497585b2c
ANDREW PURDY <andrewmp1@gmail.com>
2016-05-13 22:18:50 -0700

    Add license and update package.json

--- blueprints/ember-pdf/index.js
+++ blueprints/ember-pdf/index.js
@@ -1,15 +1,9 @@
-var path = require('path');
-
 module.exports = {
 
   normalizeEntityName: function () {},
 
   afterInstall: function () {
-    var bowerJsonPath = path.join(__dirname, '..', '..', 'bower.json');
-    var bowerJson =  require(bowerJsonPath);
-
-    return this.addBowerPackageToProject('pdfjs-dist', bowerJson.dependencies['pdfjs-dist']);
+    return this.addBowerPackageToProject('pdfjs-dist', '>=1.4.0');
   }
 
 };
-
